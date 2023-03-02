884ddf2af7a373536d7c35b0d52eff22d3ac4aeb
borjjayy <srjayy12@gmail.com>
2023-03-02 23:02:59 +0800

    few

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -12,6 +12,7 @@
     </head>
     <body>
        <div class='mainContainer'>
+        
        @yield('navBar')
     
        <h1> Hello </h1>
--- resources/views/navBar/navBar.blade.php
+++ resources/views/navBar/navBar.blade.php
@@ -3,6 +3,23 @@
 @section('navBar')
 
 
+<div>
+    <ul>
+<li>
+    hello
+</li>
+<li>
+    hello
+</li>
+<li>
+    hello
+</li>
+<li>
+    hello
+</li>
 
+
+    </ul>
+</div>
     
 @endsection
