e4a14fe0e66694a7c1601be22a37f379813fda2e
borjjayy <srjayy12@gmail.com>
2023-03-09 13:51:33 +0800

    few changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,12 +56,16 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+                              
                             </div>
                         </div>
                     </form>
